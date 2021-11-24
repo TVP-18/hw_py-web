03f98f267e134fd4e4b55ed833efa3312f0d498d
Tatiana Popova <tatiana_mail@bk.ru>
2021-11-24 15:24:25 +0700

    error migration

--- hw_6_6_Flask/models.py
+++ hw_6_6_Flask/models.py
@@ -1,27 +1,9 @@
 from sqlalchemy import exc
 
-import errors
 from app import db
 
 
-class BaseModelMixin:
-    @classmethod
-    def by_id(cls, obj_id):
-        obj = cls.query.get(obj_id)
-        if obj:
-            return obj
-        else:
-            raise errors.NotFound
-
-    def add(self):
-        db.session.add(self)
-        try:
-            db.session.commit()
-        except exc.IntegrityError:
-            raise errors.BadLuck
-
-
-class Ad(db.Model, BaseModelMixin):
+class Ad(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), index=True)
     text = db.Column(db.String(500), index=True)
