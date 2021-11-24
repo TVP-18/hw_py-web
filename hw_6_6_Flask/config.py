89d09d1b8a7e76aec84b6de4fc5cd905d265c77b
Tatiana Popova <tatiana_mail@bk.ru>
2021-11-24 15:06:54 +0700

    create models

--- hw_6_6_Flask/config.py
+++ hw_6_6_Flask/config.py
@@ -1,3 +1,3 @@
-POSTGRE_URI = 'postgresql://postrgres:super@127.0.0.1:5431/flask_test'
+POSTGRE_URI = 'postgresql://postgres:super@127.0.0.1:5431/flask_test'
 SALT = 'my_sJ15lt_#4$4344'
 SQLALCHEMY_TRACK_MODIFICATIONS = False
